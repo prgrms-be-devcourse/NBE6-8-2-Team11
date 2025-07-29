946ccc33c1ac109f0d9a5006b08d731d50f19345
정동하 <jeongdongha@jeongdonghaui-MacBookAir.local>
2025-07-29 11:57:11 +0900

    feat(be):shelter도메인 1차 구현
    # Conflicts:
    #	backend/src/main/java/com/back/domain/shelter/repository/ShelterRepository.java

--- backend/src/main/java/com/back/domain/shelter/repository/ShelterRepository.java
+++ backend/src/main/java/com/back/domain/shelter/repository/ShelterRepository.java
@@ -3,8 +3,5 @@ package com.back.domain.shelter.repository;
 import com.back.domain.shelter.entity.Shelter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ShelterRepository extends JpaRepository<Shelter, Long> {
-    Optional<Shelter> findByName(String name);
 }
