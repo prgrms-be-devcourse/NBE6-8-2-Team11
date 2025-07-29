1c378a6c5e602e1e1cddff8d79c8fb8129298d03
정동하 <jeongdongha@jeongdonghaui-MacBookAir.local>
2025-07-29 09:27:34 +0900

    기본 도메인 설계

--- backend/src/main/java/com/back/domain/notification/repository/NotificationRepository.java
+++ backend/src/main/java/com/back/domain/notification/repository/NotificationRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
     List<Notification> findByMemberOrderByCreatedAtDesc(Member member);
 
     Optional<Notification> findByIdAndMember(Long id, Member member);
