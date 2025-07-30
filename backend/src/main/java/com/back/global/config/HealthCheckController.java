e19bc1140246ffa8a4ef6c3b026520b84ecfa399
loseminho <loseminho@gmail.com>
2025-07-30 11:02:35 +0900

    fix: CrossOrigin import 누락 수정

--- backend/src/main/java/com/back/global/config/HealthCheckController.java
+++ backend/src/main/java/com/back/global/config/HealthCheckController.java
@@ -2,6 +2,7 @@ package com.back.global.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
